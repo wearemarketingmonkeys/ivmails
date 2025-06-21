3e6552e4f4438b5a1be011145409b674fa5f9aa7
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-06-21 16:59:36 +0400

    Update autoload.php
    update form

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,13 +1,13 @@
 <?php
 spl_autoload_register(function ($class) {
     $prefix = 'PHPMailer\\PHPMailer\\';
-    $base_dir = __DIR__ . '/phpmailer/phpmailer/';
+    $base_dir = __DIR__ . '/phpmailer/phpmailer';
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
     }
     $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $file = $base_dir . '/' . str_replace('\\', '/', $relative_class) . '.php';
     if (file_exists($file)) {
         require $file;
     }
