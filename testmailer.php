6c64764af466173f797eb05f2ba2d52db19fc41e
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-06-21 16:47:23 +0400

    Update testmailer.php
    update form

--- testmailer.php
+++ testmailer.php
@@ -1,8 +1,8 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
