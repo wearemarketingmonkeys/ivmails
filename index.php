022b47244cf0929639b603dfa8bd3d163718811d
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-06-21 15:08:04 +0400

    Update index.php
    update form

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: https://ivhub.com");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+$allowed_origins = [
+    "https://ivhubnew.onrender.com",
+    "https://ivhub.com",
+    "https://www.ivhub.com"
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
