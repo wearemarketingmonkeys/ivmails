feca8c7b1da2a81097da91e8e33bc82551d18a02
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-06-23 10:32:34 +0400

    Update consentemail.php
    update email

--- consentemail.php
+++ consentemail.php
@@ -35,6 +35,7 @@ try {
     $mail->addAddress('hello@ivhub.com');
     $mail->addAddress('riti@ivhub.com');
     $mail->addAddress('desk@ivhub.com');
+    $mail->addAddress('fiona@ivhub.com');
     $mail->addAddress('dipesh.macair@gmail.com');
 
     $mail->Subject = "IV Consent Form Submission | " . $_POST['patientName'];
