434a0829e3352c69ec70e18eecf94c9c2d00bb37
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-07-14 17:16:01 +0400

    update mail body

--- laserconsentemail.php
+++ laserconsentemail.php
@@ -72,41 +72,46 @@ try {
     $body .= "<p><strong>Date of Consent:</strong> " . $_POST['consentDate'] . "</p>";
     $body .= "<p><strong>Practitioner Name:</strong> " . $_POST['practitionerName'] . "</p>";
 
-    $body .= '<p>In cases involving the patients medical history, IV Wellness Lounge Clinic cannot assume responsibility. The administration of the multivitamin drip is exclusively performed in response to the patients specific request.</p>
-    <p><b>IV Wellness Lounge Clinic bears no responsibility for any financial consequences that may arise after or during the administration of the IV therapy drip treatment.</b></p>
-                        <p><b>IV Hub</b> provides facilities and personnel to assist in the performance of intravenous therapy. You have the right to be informed of the procedure, any feasible alternative options, the risks and benefits. Alternatives to intravenous therapy is oral supplementation and/or dietary and lifestyle changes. Except in emergencies, procedures are not performed until you have had an opportunity to receive such information and to give your informed consent. <b>IV Hub</b> does not claim any clinical therapeutic outcomes, and results may vary from every individual patient.</p>
-                        <p>The procedure involves inserting a needle into your vein or muscle and injecting the formula prescribed by your physician. It will be performed by or under the direction of your physician with qualified healthcare providers.</p>
-                        <p>Benefits of intravenous therapy include:</p>
-                        <ul>
-                          <li>Injectable are not affected by stomach or intestinal disease.</li>
-                          <li>Total amount of infusion is available to the tissues.</li>
-                          <li>Nutrients are focused into cells by means of a high concentration gradient.</li>
-                          <li>Higher doses of nutrients can be given than possible by mouth, without intestinal irritation.</li>
-                        </ul>
-                        <p>Risks of intravenous therapy include:</p>
-                        <ul>
-                          <li>Potential risks of pain, discomfort, bruising, infection, or inflammation of the vein/phlebitis at or near the injection site.</li>
-                          <li>Severe allergic reaction.</li>
-                        </ul>
-                        <p>Serious potential side effects could occur in the following patients:</p>
-                        <ul>
-                          <li>A genetic defect called “Glucose-6-Phosphate Dehydrogenase Deficiency”, or G6PD--deficiency, also known as “Favism”</li>
-                          <li>Patients with Chronic Renal Insufficiency, or decreased kidney function</li>
-                          <li>Patients with Congestive Heart Failure and/or Atrial Fibrillation “A--fib”</li>
-                          <li>Patients with very Low Blood Pressure, readings lower than 90 mm Hg systolic or 60 mm Hg diastolic (esp.
-Magnesium containing IV Infusions)</li>
-                          <li>Patients taking Digoxin or other Potassium--depleting drugs, diuretics, beta-agonists, or glucocorticoids; If patient is hypokalemic (esp. Magnesium containing IV Infusions)</li>
-                          <li>Unknown allergies</li>
-                          <li>Pregnant women</li>
-                        </ul>
-                        <p>You have the right to consent to or refuse any proposed treatment at any time prior to its performance.</p>
-                        <p><b>Your signature AFFIRMS that:</b></p>
-                        <ul>
-                          <li>You understand the information provided on this form and agree to the foregoing.</li>
-                          <li>The procedure(s) set forth above has been adequately explained to you by your physician.</li>
-                          <li>You have received all the information and explanation you desire concerning the procedure.</li>
-                          <li>You authorize and consent to the performance of the procedure(s).</li>
-                        </ul>';
+    $body .= '<p>In cases involving the patient\'s medical history, IV Wellness Lounge Clinic cannot assume responsibility. The administration of the multivitamin drip is exclusively performed in response to the patient\'s specific request.</p>
+<p><strong>IV Wellness Lounge Clinic bears no responsibility for any financial consequences that may arise after or during the administration of the IV therapy drip treatment.</strong></p>
+<p><strong>IV Hub</strong> provides facilities and personnel to assist in the performance of intravenous therapy. You have the right to be informed of the procedure, any feasible alternative options, the risks and benefits. Alternatives to intravenous therapy include oral supplementation and/or dietary and lifestyle changes. Except in emergencies, procedures are not performed until you have had an opportunity to receive such information and to give your informed consent. <strong>IV Hub</strong> does not claim any clinical therapeutic outcomes, and results may vary from patient to patient.</p>
+<p>The procedure involves inserting a needle into your vein or muscle and injecting the formula prescribed by your physician. It will be performed by or under the direction of your physician with qualified healthcare providers.</p>
+
+<p><strong>Benefits of intravenous therapy include:</strong></p>
+<ul>
+  <li>Injectables are not affected by stomach or intestinal disease.</li>
+  <li>Total amount of infusion is available to the tissues.</li>
+  <li>Nutrients are focused into cells by means of a high concentration gradient.</li>
+  <li>Higher doses of nutrients can be given than possible by mouth, without intestinal irritation.</li>
+</ul>
+
+<p><strong>Risks of intravenous therapy include:</strong></p>
+<ul>
+  <li>Potential risks of pain, discomfort, bruising, infection, or inflammation of the vein/phlebitis at or near the injection site.</li>
+  <li>Severe allergic reaction.</li>
+</ul>
+
+<p><strong>Serious potential side effects could occur in the following patients:</strong></p>
+<ul>
+  <li>A genetic defect called "Glucose-6-Phosphate Dehydrogenase Deficiency", or G6PD-deficiency, also known as "Favism"</li>
+  <li>Patients with Chronic Renal Insufficiency, or decreased kidney function</li>
+  <li>Patients with Congestive Heart Failure and/or Atrial Fibrillation (A-fib)</li>
+  <li>Patients with very low blood pressure (e.g., lower than 90/60 mm Hg), especially with Magnesium-containing IV infusions</li>
+  <li>Patients taking Digoxin or other potassium-depleting drugs, diuretics, beta-agonists, or glucocorticoids - particularly if hypokalemic (especially with Magnesium IV infusions)</li>
+  <li>Patients with unknown allergies</li>
+  <li>Pregnant women</li>
+</ul>
+
+<p>You have the right to consent to or refuse any proposed treatment at any time prior to its performance.</p>
+
+<p><strong>Your signature AFFIRMS that:</strong></p>
+<ul>
+  <li>You understand the information provided on this form and agree to the foregoing.</li>
+  <li>The procedure(s) described above have been adequately explained to you by your physician.</li>
+  <li>You have received all the information and explanation you desire concerning the procedure.</li>
+  <li>You authorize and consent to the performance of the procedure(s).</li>
+</ul>';
+
                         
     $body .= "<hr><p><strong>Submitted via Laser Hair Removal Consent Form on IVHUB</strong></p>";
     $body .= "</body></html>";
